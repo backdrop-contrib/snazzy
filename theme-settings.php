313056482332c4ff9a1f1948356839f9c5bd9f55
indigoxela <user@olga.indigofloat.at>
2020-12-18 16:38:03 +0100

    Color module support, settings form

---
+++ theme-settings.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @file
+ *
+ * Theme settings.
+ */
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function snazzy_form_system_theme_settings_alter(&$form, &$form_state) {
+  // Settings for color module.
+  if (module_exists('color')) {
+    $form['base'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Base Settings'),
+      '#collapsible' => TRUE,
+    );
+    $fields = array(
+      'base',
+      'text',
+      'link',
+      'borders',
+      'boxbg',
+      'brightbg',
+      'lightbg',
+      'darkbg',
+    );
+    foreach ($fields as $field) {
+      $form['base'][$field] = color_get_color_element($form['theme']['#value'], $field, $form);
+    }
+
+    $form['additional'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Header, Buttons, Footer'),
+      '#collapsible' => TRUE,
+    );
+    $fields = array(
+      'header',
+      'headertext',
+      'herobg',
+      'herotext',
+      'buttons',
+      'buttonsborder',
+      'buttonstext',
+      'footer',
+      'footertext',
+    );
+    foreach ($fields as $field) {
+      $form['additional'][$field] = color_get_color_element($form['theme']['#value'], $field, $form);
+    }
+
+  }
+  else {
+    $form['color'] = array(
+      '#markup' => '<p>' . t('This theme supports custom color palettes if the Color module is enabled on the <a href="!url">modules page</a>. Enable the Color module to customize this theme.', array('!url' => url('admin/modules'))) . '</p>',
+    );
+  }
+  // Other settings.
+  $form['settings'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Other Settings'),
+    '#collapsible' => TRUE,
+    '#description' => t('Note: you can not preview this here, you first have to save it. The preview frame is too small, follow the link to open the preview in a new window.'),
+  );
+  $form['settings']['front_header_split'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Front page header split'),
+    '#default_value' => theme_get_setting('front_header_split', 'snazzy'),
+    '#description' => t('Show the header and top region on the start page side by side.'),
+  );
+}
